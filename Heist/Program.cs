56ad7db5577c853d8e17dd0c15c6ff013ebfc298
Fabian Lindgren <lindgrenfabian@gmail.com>
2021-12-13 23:47:37 +0100

    Remove unnecessary namespaces

--- Heist/Program.cs
+++ Heist/Program.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net;
-using System.Collections.Specialized;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace Heist
 {
